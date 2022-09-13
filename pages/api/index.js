5195b4a80438960101dc4567088b1413c0751529
Kiko Beats <josefrancisco.verdu@gmail.com>
2022-09-13 16:19:11 -0700

    build: first commit

---
+++ pages/api/index.js
@@ -0,0 +1,28 @@
+'use strict'
+
+const toArray = (input, delimiter = '/n') => [].concat(input).join(delimiter)
+
+const composeHtml = ({ head, body }) =>
+  `<!DOCTYPE html>
+<html lang="en">
+<head>${toArray(head)}</head>
+<body>${toArray(body)}</body>
+</html>`.trim()
+
+export const config = { runtime: 'experimental-edge' }
+
+export default async request => {
+  const url = request.nextUrl
+
+  if (url.pathname === '/favicon.ico' || url.pathname === '/robots.txt') {
+    return new Response()
+  }
+
+  const html = composeHtml(Object.fromEntries(url.searchParams))
+
+  return new Response(html, {
+    headers: {
+      'Content-Type': 'text/html; charset=UTF-8'
+    }
+  })
+}
